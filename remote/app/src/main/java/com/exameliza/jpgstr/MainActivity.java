07901dabc30f90425d2b7c0b1d1bf81a68cfb499
PVVovan <pvvovan@gmail.com>
2021-05-29 11:50:58 +0200

    Reduce camera image update rate

--- remote/app/src/main/java/com/exameliza/jpgstr/MainActivity.java
+++ remote/app/src/main/java/com/exameliza/jpgstr/MainActivity.java
@@ -201,7 +201,7 @@ public class MainActivity extends AppCompatActivity {
                     e.printStackTrace();
                 }
 
-                SystemClock.sleep(15);
+                SystemClock.sleep(1000);
             }
         }
     }
